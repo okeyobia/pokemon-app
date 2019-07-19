a02b03c1a67583f3e0d1ed28f185f312e851e410
okey <okey.obia@gmail.com>
2019-07-19 02:33:47 -0400

    added pokegame component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,31 +1,30 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
+import "./Pokedex.css";
 
 export default class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
-    ]
-  };
   render() {
+    let pokedexTitle;
+    if (this.props.isWinner) {
+      pokedexTitle = <h1 className="Pokedex-winner">Pokemon Winner!!</h1>;
+    } else {
+      pokedexTitle = <h1 className="Pokedex-loser">Pokemon Loser</h1>;
+    }
     return (
-      <div>
-        {this.props.pokemon.map((poke, key) => (
-          <Pokecard
-            key={poke.id}
-            id={poke.id}
-            name={poke.name}
-            exp={poke.base_experience}
-            type={poke.type}
-          />
-        ))}
+      <div className="Pokedex">
+        <p>Total Experience: {this.props.exp}</p>
+        {pokedexTitle}
+        <div className="Pokedex-card">
+          {this.props.pokemon.map((poke, key) => (
+            <Pokecard
+              key={poke.id}
+              id={poke.id}
+              name={poke.name}
+              exp={poke.base_experience}
+              type={poke.type}
+            />
+          ))}
+        </div>
       </div>
     );
   }
